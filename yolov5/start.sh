ea7f54a2b1ebe6e0e2fa79ce66b8540cbb65f390
ubuntu <ubuntu@localhost.localdomain>
2022-11-07 15:53:26 +0800

    set up env && clean project

---
+++ yolov5/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -v
+# test.sh
+echo "####################################"
+echo "#                                  #"
+echo "#                                  #"
+echo "#                                  #"
+echo "####################################"
+echo "Please pick a name for this train: "
+read name
+echo "$name"
