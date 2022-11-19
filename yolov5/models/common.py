a9ea9a5ae0ea472dffeac264474a5b994b783530
PionC <99478673+PinhengChen@users.noreply.github.com>
2022-11-19 23:31:36 +1100

    Update common.py

--- yolov5/models/common.py
+++ yolov5/models/common.py
@@ -861,6 +861,11 @@ class Classify(nn.Module):
       
       
 class SE(nn.Module):
+  """
+  Reference: [Squeeze-and-Excitation Networks]
+  Links: https://arxiv.org/pdf/1709.01507.pdf
+         https://blog.csdn.net/weixin_43694096/article/details/124443059
+  """
     def __init__(self, c1, c2, ratio=16):
         super(SE, self).__init__()
         #c*1*1
