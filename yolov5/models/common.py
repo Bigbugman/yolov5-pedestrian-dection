cb518aa8c9dfb8366a540efbbc62570d06242032
PionC <99478673+PinhengChen@users.noreply.github.com>
2022-11-19 21:17:47 +1100

    Update common.py
    SPD updated.

--- yolov5/models/common.py
+++ yolov5/models/common.py
@@ -914,3 +914,13 @@ class ECA(nn.Module):
         y = self.sigmoid(y)
 
         return x * y.expand_as(x)
+      
+ 
+class space_to_depth(nn.Module):
+    # Changing the dimension of the Tensor
+    def __init__(self, dimension=1):
+        super().__init__()
+        self.d = dimension
+
+    def forward(self, x):
+         return torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
